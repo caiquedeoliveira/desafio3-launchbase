7a5035bd3125badacc20e1f0902ff4c340bd2cb4
Caique Oliveira <caiquerodrigues@id.uff.br>
2020-06-30 10:21:56 -0300

    pasta publica

---
+++ public/script/course.js
@@ -0,0 +1,9 @@
+
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards){
+    card.addEventListener('click', () => {
+        const courseId = card.getAttribute('id')
+        window.location.href = `https://rocketseat.com.br/${courseId}`
+    })
+}
